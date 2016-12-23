02b60104aa52008cc184a6573c9a9d7095911d02
Jamie Seward <jamie.d.seward@gmail.com>
2016-12-22 23:51:07 -0800

    Day23 part2

--- day23.py
+++ day23.py
@@ -13,8 +13,14 @@ def is_register(x):
 assert is_register('a')
 assert not is_register('1')
 
-VERBOSE = True
+VERBOSE = False
 verbose_file = open('day23.output', 'wt') if VERBOSE else None
+
+def is_verbose(state):
+    return verbose_file
+
+def state_to_string(state):
+    return "a={} b={} c={} d={}".format(state['registers']['a'], state['registers']['b'], state['registers']['c'], state['registers']['d'])
 
 def cpy(x, y, state, is_tgl):
     if is_tgl:
@@ -29,40 +35,30 @@ def cpy(x, y, state, is_tgl):
     except ValueError:
         value = state['registers'][x] 
 
-    if verbose_file:
-        verbose_file.write("{}: cpy {} ({}) -> {}\n".format(state['instruction'], x, value, y))
+    if is_verbose(state):
+        verbose_file.write("{}: cpy {} ({}) -> {} : {}\n".format(state['instruction'], x, value, y, state_to_string(state)))
 
     state['registers'][y] = value
     state['instruction'] += 1
 
-PART_TWO = True
-
 def inc(x, state, is_tgl):
     if is_tgl:
-        if PART_TWO:
-            mul(x, state, False)
-            return
-        else:
-            dec(x, state, False)
-            return
+        dec(x, state, False)
+        return
 
     state['registers'][x] += 1
-    if verbose_file:
-        verbose_file.write("{}: inc {} -> ({})\n".format(state['instruction'], x, state['registers'][x]))
+    if is_verbose(state):
+        verbose_file.write("{}: inc {} -> ({}) : {}\n".format(state['instruction'], x, state['registers'][x], state_to_string(state)))
     state['instruction'] += 1
 
 def dec(x, state, is_tgl):
-    #if PART_TWO:
-        #div(x, state, is_tgl)
-        #return
-
     if is_tgl:
         inc(x, state, False)
         return
 
     state['registers'][x] -= 1
-    if verbose_file:
-        verbose_file.write("{}: dec {} -> ({})\n".format(state['instruction'], x, state['registers'][x]))
+    if is_verbose(state):
+        verbose_file.write("{}: dec {} -> ({}) : {}\n".format(state['instruction'], x, state['registers'][x], state_to_string(state)))
     state['instruction'] += 1
 
 def jnz(x, y, state, is_tgl):
@@ -75,8 +71,8 @@ def jnz(x, y, state, is_tgl):
     except ValueError:
         is_not_zero = (state['registers'][x] != 0)
 
-    if verbose_file:
-        verbose_file.write("{}: jnz {} {}\n".format(state['instruction'], is_not_zero, y))
+    if is_verbose(state):
+        verbose_file.write("{}: jnz {} {} {}: {}\n".format(state['instruction'], x, is_not_zero, y, state_to_string(state)))
 
     if is_not_zero:
         try:
@@ -86,20 +82,16 @@ def jnz(x, y, state, is_tgl):
     else:
         state['instruction'] += 1
 
-def mul(x, state, is_tgl):
-    if is_tgl:
-        dec(x, state, False)
-        return
+def mul(x, y, state, is_tgl):
+    assert not is_tgl
+    assert is_register(x)
+    assert is_register(y)
 
-    state['registers'][x] *= 2
+    state['registers'][x] *= state['registers'][y]
     state['instruction'] += 1
 
-def div(x, state, is_tgl):
-    if is_tgl:
-        inc(x, state, False)
-        return
-    
-    state['registers'][x] /= 2
+def nul(state, is_tgl):
+    assert not is_tgl
     state['instruction'] += 1
 
 def tgl(x, state, is_tgl):
@@ -115,8 +107,8 @@ def tgl(x, state, is_tgl):
     tgl_instruction = state['instruction'] + value
     state['tgl'][tgl_instruction] = True
 
-    if verbose_file:
-        verbose_file.write("{}: tgl {}\n".format(state['instruction'], value))
+    if is_verbose(state):
+        verbose_file.write("{}: tgl {} : {}\n".format(state['instruction'], value, state_to_string(state)))
 
     state['instruction'] += 1
 
@@ -140,6 +132,14 @@ def parse_op(s):
     tgl_search = re.search("tgl (\S+)", s)
     if tgl_search:
         return partial(tgl, tgl_search.group(1))
+
+    mul_search = re.search("mul (\S+) (\S+)", s)
+    if mul_search:
+        return partial(mul, mul_search.group(1), mul_search.group(2))
+
+    nul_search = re.search("nul", s)
+    if nul_search:
+        return partial(nul)
         
     raise ValueError(s)
 
@@ -179,7 +179,7 @@ def run_part_1():
     print run(op_strings, reg_init={'a': 7}) 
 
 def run_part_2():
-    with open('day23.input', 'rt') as f:
+    with open('day23.part2.input', 'rt') as f:
         op_strings = [line.strip() for line in f.readlines()]
     print run(op_strings, reg_init={'a': 12}) 
 
